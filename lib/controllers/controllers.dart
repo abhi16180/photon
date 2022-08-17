ebc1773e4094817c9f7efd4956a69b9f2e6697e5
abhi16180 <4ni19cs003_a@nie.ac.in>
2022-08-17 14:13:33 +0530

    cancel and restart feature added

--- lib/controllers/controllers.dart
+++ lib/controllers/controllers.dart
@@ -1,7 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
-
 
 class PercentageController extends GetxController {
   var percentage = [].obs;
+  var isCancelled = [].obs;
+  List<CancelToken> cancelTokenList = [];
 }
-
