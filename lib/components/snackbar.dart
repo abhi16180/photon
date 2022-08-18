0d08fce358ae65da9573065df1dc6ae3c283c55f
abhi16180 <hegdeabhilash19@gmail.com>
2022-08-18 11:51:53 +0530

    update

--- lib/components/snackbar.dart
+++ lib/components/snackbar.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 showSnackBar(context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      backgroundColor: const Color.fromARGB(255, 0, 3, 30),
+      backgroundColor: Colors.blueGrey.shade900,
       behavior: SnackBarBehavior.floating,
       margin: const EdgeInsets.all(50),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       elevation: 30,
       content: Text(
         content,
