040a24844b21e15861e612c74a696894bdaf73c4
abhi16180 <hegdeabhilash19@gmail.com>
2022-08-26 12:06:30 +0530

    Show "downloaded" status Fixes #21

--- lib/controllers/controllers.dart
+++ lib/controllers/controllers.dart
@@ -4,5 +4,6 @@ import 'package:get/get.dart';
 class PercentageController extends GetxController {
   var percentage = [].obs;
   var isCancelled = [].obs;
+  var isReceived = [].obs;
   List<CancelToken> cancelTokenList = [];
 }
