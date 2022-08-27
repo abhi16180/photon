efe031a61955e90227a8e663d594f5dd8c4f7d7d
abhi16180 <4ni19cs003_a@nie.ac.in>
2022-08-27 23:10:18 +0530

    update

--- lib/views/widescreen_home.dart
+++ lib/views/widescreen_home.dart
@@ -15,15 +15,17 @@ class _WidescreenHomeState extends State<WidescreenHome> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        if (!isloading) ...{
-          Center(
-            child: Card(
-              child: MaterialButton(
-                onPressed: () async {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (!isloading) ...{
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(28)),
+              child: InkWell(
+                onTap: () async {
                   setState(() {
                     isloading = true;
                   });
@@ -54,55 +56,56 @@ class _WidescreenHomeState extends State<WidescreenHome> {
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            width: size.width / 10,
-          ),
-          Card(
-            child: MaterialButton(
-              clipBehavior: Clip.hardEdge,
-              onPressed: () {
-                Navigator.of(context).pushNamed('/receivepage');
-              },
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Lottie.asset('assets/lottie/receive-file.json',
-                      width: size.width / 4, height: size.height / 4),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      'Receive',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+            SizedBox(
+              width: size.width / 10,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(28)),
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).pushNamed('/receivepage');
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Lottie.asset('assets/lottie/receive-file.json',
+                        width: size.width / 4, height: size.height / 4),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Receive',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        } else ...{
-          Center(
-            child: SizedBox(
-              width: size.width / 4,
-              height: size.height / 4,
-              child: Lottie.asset('assets/lottie/setting_up.json',
-                  width: 40, height: 40),
-            ),
-          ),
-          const Center(
-            child: Text(
-              'Please wait !',
-              style: TextStyle(
-                fontSize: 20,
+          } else ...{
+            Center(
+              child: SizedBox(
+                width: size.width / 4,
+                height: size.height / 4,
+                child: Lottie.asset('assets/lottie/setting_up.json',
+                    width: 40, height: 40),
               ),
-              textAlign: TextAlign.center,
             ),
-          )
-        }
-      ],
+            const Center(
+              child: Text(
+                'Please wait !',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            )
+          }
+        ],
+      ),
     );
   }
 }
