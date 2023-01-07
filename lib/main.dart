0d67648dce1516e9f5a6b49cac1f7f7a0058a9a2
abhi16180 <hegdeabhilash19@gmail.com>
2023-01-07 12:39:38 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,7 @@ void main() async {
                   nextScreen: prefInst.getBool('isIntroRead') == true
                       ? (externalIntent ? const HandleIntentUI() : const App())
                       : const IntroPage(),
+                  duration: 1000,
                   splashTransition: SplashTransition.fadeTransition,
                   pageTransitionType: PageTransitionType.fade,
                   backgroundColor: const Color.fromARGB(255, 0, 4, 7),
