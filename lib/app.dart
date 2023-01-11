a5c1c7f0d9f8d89c1b152c66c4e9e8ccf31fd77c
abhi16180 <hegdeabhilash19@gmail.com>
2023-01-11 12:31:14 +0530

    add profile

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hive/hive.dart';
 import 'package:photon/views/drawer/about_page.dart';
 import 'package:photon/views/drawer/settings.dart';
 import 'package:photon/views/home/widescreen_home.dart';
@@ -28,6 +29,7 @@ class _AppState extends State<App> {
     super.initState();
   }
 
+  Box box = Hive.box('appData');
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -69,97 +71,131 @@ class _AppState extends State<App> {
                 })
               },
               child: Drawer(
-                child: ListView(
+                child: Stack(
                   children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(0.0),
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'assets/images/icon.png',
-                            width: 75,
-                            height: 75,
+                    ListView(
+                      shrinkWrap: true,
+                      children: [
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                box.get('avatarPath'),
+                                width: 90,
+                                height: 90,
+                              ),
+                              Text(
+                                box.get('username'),
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
                           ),
-                          const Text('Photon')
-                        ],
-                      ),
-                    ),
-                    ListTile(
-                      leading: Icon(
-                        UniconsSolid.history,
-                        color: mode.isDark ? null : Colors.black,
-                      ),
-                      title: const Text('Received-history'),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const HistoryPage();
-                            },
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Divider(
+                            height: 2,
+                            color: Colors.blueGrey,
                           ),
-                        );
-                      },
-                    ),
-                    ListTile(
-                      leading: Icon(
-                        Icons.settings,
-                        color: mode.isDark ? null : Colors.black,
-                      ),
-                      title: const Text("Settings"),
-                      onTap: () {
-                        Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (context) {
-                          return const SettingsPage();
-                        }));
-                      },
-                    ),
-                    ListTile(
-                      leading: SvgPicture.asset(
-                        'assets/icons/licenses.svg',
-                        color: mode.isLight ? Colors.black : Colors.white,
-                      ),
-                      onTap: () {
-                        showLicensePage(
-                            context: context,
-                            applicationLegalese: 'GPL3 license',
-                            applicationVersion: "1.1.1",
-                            applicationIcon: Image.asset(
-                              'assets/images/splash.png',
-                              width: 60,
-                            ));
-                      },
-                      title: const Text('Licenses'),
-                    ),
-                    ListTile(
-                      leading: Icon(
-                        Icons.privacy_tip_rounded,
-                        color: mode.isDark ? null : Colors.black,
-                      ),
-                      onTap: () async {
-                        launchUrl(
-                            Uri.parse(
-                                "https://photondev.netlify.app/privacy-policy-page"),
-                            mode: LaunchMode.externalApplication);
-                      },
-                      title: const Text('Privacy policy'),
-                    ),
-                    ListTile(
-                      title: const Text('About'),
-                      leading: Icon(UniconsLine.info_circle,
-                          color: mode.isDark ? null : Colors.black),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return const AboutPage();
-                            },
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            UniconsSolid.history,
+                            color: mode.isDark ? null : Colors.black,
                           ),
-                        );
-                      },
+                          title: const Text('Received-history'),
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const HistoryPage();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.settings,
+                            color: mode.isDark ? null : Colors.black,
+                          ),
+                          title: const Text("Settings"),
+                          onTap: () {
+                            Navigator.of(context)
+                                .push(MaterialPageRoute(builder: (context) {
+                              return const SettingsPage();
+                            }));
+                          },
+                        ),
+                        ListTile(
+                          leading: SvgPicture.asset(
+                            'assets/icons/licenses.svg',
+                            color: mode.isLight ? Colors.black : Colors.white,
+                          ),
+                          onTap: () {
+                            showLicensePage(
+                                context: context,
+                                applicationLegalese: 'GPL3 license',
+                                applicationVersion: "1.1.1",
+                                applicationIcon: Image.asset(
+                                  'assets/images/splash.png',
+                                  width: 60,
+                                ));
+                          },
+                          title: const Text('Licenses'),
+                        ),
+                        ListTile(
+                          leading: Icon(
+                            Icons.privacy_tip_rounded,
+                            color: mode.isDark ? null : Colors.black,
+                          ),
+                          onTap: () async {
+                            launchUrl(
+                                Uri.parse(
+                                    "https://photondev.netlify.app/privacy-policy-page"),
+                                mode: LaunchMode.externalApplication);
+                          },
+                          title: const Text('Privacy policy'),
+                        ),
+                        ListTile(
+                          title: const Text('About'),
+                          leading: Icon(UniconsLine.info_circle,
+                              color: mode.isDark ? null : Colors.black),
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const AboutPage();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
+                    Positioned(
+                      bottom: 18,
+                      right: 5,
+                      child: Padding(
+                        padding: const EdgeInsets.all(0.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/icon.png',
+                                width: 30, height: 30),
+                            const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text('Photon v 1.1.1'),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
