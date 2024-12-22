c6a0adcd885536d2b8b2e2b88bbf54d8f2d5b9ee
Abhilash Hegde <hegdeabhilash19@gmail.com>
2024-12-22 21:23:52 +0530

    remove log

--- lib/services/photon_sender.dart
+++ lib/services/photon_sender.dart
@@ -138,7 +138,6 @@ class PhotonSender {
               paths = await FileUtils.listFilesForPickedDir(dir);
             }
             _parentFolder = selectedDirectory;
-            print("parent is ${_parentFolder}");
             _fileList = paths;
           } else {
             String? dirPath = await FilePicker.platform.getDirectoryPath();
